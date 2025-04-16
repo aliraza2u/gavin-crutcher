8dac798abe0e42a0bb7527737f40200fd27a8221
Ali Raza <raza8r@gmail.com>
2025-04-17 02:01:23 +0500

    removed song name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 "use client";
 import Image from "next/image";
 
-import { Libre_Caslon_Display, Roboto } from "next/font/google";
+import {
+  //  Libre_Caslon_Display,
+  Roboto,
+} from "next/font/google";
 
-const caslon = Libre_Caslon_Display({
-  weight: "400",
-  subsets: ["latin"],
-});
+// const caslon = Libre_Caslon_Display({
+//   weight: "400",
+//   subsets: ["latin"],
+// });
 
 const roboto = Roboto({
   weight: ["400", "600", "700"],
@@ -16,24 +19,24 @@ const roboto = Roboto({
 export default function HeroSection() {
   return (
     <section className="relative w-full h-screen overflow-hidden bg-[url(/gavin-crutcher-cover-lg.jpeg)] sm:bg-[url(/gavin-crutcher-cover-md.jpeg)] xl:bg-[url(/gavin-crutcher-cover-lg.jpeg)] bg-center sm:bg-left xl:bg-center bg-cover bg-no-repeat">
-      <div className="absolute top-[6%] inset-x-0 sm:inset-x-auto sm:top-[10%] sm:left-[42%] md:left-[50%] xl:left-[63%] text-white z-10 max-w-md flex flex-col items-center sm:bg-[#ffffff4d] py-7 px-8 rounded-xl">
+      <div className="absolute top-[6%] inset-x-0 sm:inset-x-auto sm:top-[10%] sm:left-[42%] md:left-[50%] xl:left-[63%] text-white z-10 max-w-md flex flex-col items-center sm:bg-[#ffffff4d] py-9 px-10 rounded-xl">
         <Image
           src="/logo.png"
           alt="Gavin Crutcher"
           width={300}
           height={200}
-          className="mb-3 hidden sm:block"
+          className="mb-10 hidden sm:block"
         />
         <Image
           src="/logo.png"
           alt="Gavin Crutcher"
           width={230}
           height={200}
-          className="mb-3 block sm:hidden"
+          className="mb-5 block sm:hidden"
         />
-        <p className={`${caslon.className} text-2xl mb-6 italic text-center text-[#443830]`}>
+        {/* <p className={`${caslon.className} text-2xl mb-6 italic text-center text-[#443830]`}>
           Days That I Miss
-        </p>
+        </p> */}
         <a
           href="https://www.linktree.com/gavincrutcher"
           target="_blank"
@@ -47,7 +50,7 @@ export default function HeroSection() {
         href="https://www.linktree.com/gavincrutcher"
         target="_blank"
         rel="noopener noreferrer"
-        className={`${roboto.className} text-center w-[75%] animate__heartBeat sm:hidden  absolute bottom-[4%] right-[10%] text-lg tracking-wide px-5 py-1.5 rounded-full font-semibold  bg-[#a36b2a] border border-[#b3640c] text-white cursor-pointer transition active:scale-95`}
+        className={`${roboto.className} text-center w-[75%] animate__heartBeat sm:hidden absolute bottom-[5%] right-[10%] text-lg tracking-wide px-5 py-1.5 rounded-full font-semibold  bg-[#a36b2a] border border-[#b3640c] text-white cursor-pointer transition active:scale-95`}
       >
         Listen Now
       </a>
