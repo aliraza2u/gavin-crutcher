8a4fe95efe0e4c8435dc8b409343c74e3d5d532e
Ali Raza <raza8r@gmail.com>
2025-04-17 01:19:53 +0500

    designed header section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,56 @@
+"use client";
 import Image from "next/image";
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+import { Libre_Caslon_Display, Roboto } from "next/font/google";
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+const caslon = Libre_Caslon_Display({
+  weight: "400",
+  subsets: ["latin"],
+});
+
+const roboto = Roboto({
+  weight: ["400", "600", "700"],
+  subsets: ["latin"],
+});
+
+export default function HeroSection() {
+  return (
+    <section className="relative w-full h-screen overflow-hidden bg-[url(/gavin-crutcher-cover-lg.jpeg)] sm:bg-[url(/gavin-crutcher-cover-md.jpeg)] xl:bg-[url(/gavin-crutcher-cover-lg.jpeg)] bg-center sm:bg-left xl:bg-center bg-cover bg-no-repeat">
+      <div className="absolute top-[6%] inset-x-0 sm:inset-x-auto sm:top-[10%] sm:left-[42%] md:left-[50%] xl:left-[63%] text-white z-10 max-w-md flex flex-col items-center sm:bg-[#ffffff4d] py-7 px-8 rounded-xl">
+        <Image
+          src="/logo.png"
+          alt="Gavin Crutcher"
+          width={300}
+          height={200}
+          className="mb-3 hidden sm:block"
+        />
+        <Image
+          src="/logo.png"
+          alt="Gavin Crutcher"
+          width={230}
+          height={200}
+          className="mb-3 block sm:hidden"
+        />
+        <p className={`${caslon.className} text-2xl mb-6 italic text-center text-[#443830]`}>
+          Days That I Miss
+        </p>
         <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://www.linktree.com/gavincrutcher"
           target="_blank"
           rel="noopener noreferrer"
+          className={`${roboto.className} hidden sm:block animate__heartBeat text-lg tracking-wide bg-[#a36b2a] border border-[#b3640c] px-6 py-1.5 rounded-full font-semibold text-white cursor-pointer transition active:scale-95`}
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
+          Listen Now
         </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+      <a
+        href="https://www.linktree.com/gavincrutcher"
+        target="_blank"
+        rel="noopener noreferrer"
+        className={`${roboto.className} text-center w-[75%] animate__heartBeat sm:hidden  absolute bottom-[4%] right-[10%] text-lg tracking-wide px-5 py-1.5 rounded-full font-semibold  bg-[#a36b2a] border border-[#b3640c] text-white cursor-pointer transition active:scale-95`}
+      >
+        Listen Now
+      </a>
+    </section>
   );
 }
